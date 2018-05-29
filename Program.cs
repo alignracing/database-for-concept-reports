624737f8d24350d52b0fc1ee0780a2abbeffa8aa
Roman Aleksandrov <romana16@uia.no>
2018-05-29 18:09:54 +0200

    set up Program.cs to be able to run project

--- Program.cs
+++ Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using database_for_concept_reports.Data;
+using database_for_concept_reports.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace database_for_concept_reports
@@ -14,7 +18,22 @@ namespace database_for_concept_reports
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var env = services.GetRequiredService<IHostingEnvironment>();
+
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                
+
+                ApplicationDbInitializer.Initialize(context, userManager, env.IsDevelopment());
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
